3bf7039847bd12fcba2d81c8a9d8eb2c353bd303
Kaique Ricardo de Melo <kaikricardo99@gmail.com>
2024-08-30 12:30:19 -0300

    add submenu styles

--- src/components/home/submenu/index.tsx
+++ src/components/home/submenu/index.tsx
@@ -1,19 +1,51 @@
+"use client";
+
 import Link from "next/link";
 import styles from "./styles.module.scss";
 
 import { X, Menu } from "lucide-react";
+import { useEffect, useState } from "react";
 
 export function Submenu() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function toggleMenu() {
+    setIsOpen(!isOpen);
+  }
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth > 768) {
+        setIsOpen(false);
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
   return (
     <section className={styles.submenu}>
-      <div className={styles.submenuIcon}>
+      <div onClick={toggleMenu} className={styles.submenuIcon}>
         <Menu size={34} color="#121212" />
         Menu
       </div>
-      
-      <ul className={styles.ul}>
+
+      <ul className={`${styles.ul} ${isOpen ? styles.open : ""}`}>
+        {isOpen &&
+          <button>
+            <X
+              onClick={toggleMenu}
+              size={54}
+              color="#121212"
+              className={styles.closeButton}
+            />
+          </button>}
+
         <li>
           <Link href={"/post/pagina-1"}>Página 1</Link>
+        </li>
+        <li>
           <Link href={"/post/pagina-1"}>Página 2</Link>
         </li>
       </ul>

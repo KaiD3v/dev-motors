b8d40a5438eb837537255e7053682bef68cb37d6
Kaique Ricardo de Melo <kaikricardo99@gmail.com>
2024-09-07 18:01:35 -0300

    add hero component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,23 @@ import styles from "./page.module.css";
 import { Submenu } from "../components/home/submenu";
 import { getDataHome } from "../utils/actions/get-data";
 import { HomeProps } from "../utils/home.type";
+import { Hero } from "../components/hero";
+import { Phone } from "lucide-react";
 
 export default async function Home() {
-  const data: HomeProps = await getDataHome();
+  const { object }: HomeProps = await getDataHome();
 
-  console.log(JSON.stringify(data, null, 2));
+  console.log(JSON.stringify(object, null, 2));
   return (
     <main>
       <Submenu />
+      <Hero
+        heading={object.metadata.heading}
+        buttonTitle={object.metadata.cta_button.title}
+        buttonUrl={object.metadata.cta_button.url}
+        bannerUrl={object.metadata.banner.url}
+        icon={<Phone size={24} color="#FFF" />}
+      />
     </main>
   );
 }

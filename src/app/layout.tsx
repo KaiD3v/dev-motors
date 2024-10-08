3087e470b55b919cacef89375a63bccf4c207fc9
Kaique Ricardo de Melo <kaikricardo99@gmail.com>
2024-10-07 22:15:53 -0300

    add metadata to better SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,21 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "DevMotors - sua oficina especializada!",
   description: "Oficina de carros do interior de Quaraguatatuba!",
+  keywords: ["oficina", "oficina carros", "manutenção de carros"],
+  openGraph: {
+    title: "DevMotors - Sua oficina especializada!",
+    images: [`${process.env.NEXT_PUBLICURL}/logo.jpg`]
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      noimageindex: true
+    }
+  }
 };
 
 export default function RootLayout({
@@ -17,7 +32,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+      <body className={inter.className}>
         <Header />
         {children}
 
